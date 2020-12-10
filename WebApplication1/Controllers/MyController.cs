a03d52b4eebdc98e0ec24db1e07015d62f6217de
Liza Cher <lady.cherepko@gmail.com>
2020-12-10 15:39:49 +0300

    Add project files.

---
+++ WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController
+    {
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult FirstPost(string s)
+        {
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create("http://localhost:13693");
+            httpReq.AllowAutoRedirect = false;
+
+            HttpWebResponse httpRes = (HttpWebResponse)httpReq.GetResponse();
+            
+            if (httpRes.StatusCode == HttpStatusCode.Moved)
+            {
+                return this.Content(HttpStatusCode.OK, new { response = "Hello" });
+            }
+            
+            else return this.Content(HttpStatusCode.BadRequest, new { response = "Error" }); ;
+            
+            // Close the response.
+            httpRes.Close(); 
+        }
+
+        private ActionResult Content(HttpStatusCode oK, object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
